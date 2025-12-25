5bac713cae26bb1f2663d85a3f42e7371f0a8185
yashpanchal01 <yashpanchal251198@gmail.com>
2025-12-25 09:41:06 +0530

    Progress

--- hangman.py
+++ hangman.py
@@ -1,1 +1,28 @@
-print("works!")
+import random 
+
+words = {'apple', 'orange', 'banana', 'coconut', 'watermelon'}
+
+Hangman_art = {0:('   ',
+                 '   ',
+                 '   '),
+               1:(' O ',
+                 '   ',
+                 '   '),
+               2:(' O ',
+                 ' | ',
+                 '   '),
+               3:(' O ',
+                  '/| ',
+                 '   '),
+               4:(' O ',
+                  '/|\\',
+                 '    '), 
+               5:(' O ',
+                  '/|\\',
+                  '  \\'), 
+               6:('  O ',
+                 ' /|\\',
+                 ' / \\')}
+
+for line in Hangman_art[1]:
+    print(line)
