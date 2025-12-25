da5632d11f012af36911ccb41842eb89dfbd5df2
yashpanchal01 <yashpanchal251198@gmail.com>
2025-12-25 17:46:01 +0530

    Completed!

---
+++ Somefile.py
@@ -0,0 +1,4 @@
+from hangman import display_hangman
+
+display_hangman()
+print("Hellow")
--- hangman.py
+++ hangman.py
@@ -1,6 +1,6 @@
 import random 
 
-words = {'apple', 'orange', 'banana', 'coconut', 'watermelon'}
+words = ('apple', 'orange', 'banana', 'coconut', 'watermelon')
 
 Hangman_art = {0:('   ',
                  '   ',
@@ -24,5 +24,69 @@ Hangman_art = {0:('   ',
                  ' /|\\',
                  ' / \\')}
 
-for line in Hangman_art[1]:
-    print(line)
+def display_hangman(Wrong_guesses):
+    print('\n----------------')
+    for line in Hangman_art[Wrong_guesses]:
+        print(line)
+    print('----------------')
+    
+
+def display_hint(hint):
+    result = ''.join(hint)
+    print(result)
+
+
+def main():
+    answer = random.choice(words)
+    # hint = '_' * len(answer)
+    # hint = list(hint)
+    hint = ['_'] * len(answer)
+
+    Wrong_guesses = 0
+    is_running = True 
+    # print(hint)
+    guessed_letters = set()
+
+    
+    while is_running:
+        display_hangman(Wrong_guesses)
+        display_hint(hint)
+        
+        guess = str(input('Enter your Guess: '))
+        
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input!")
+            continue
+        
+        if guess in guessed_letters:
+            print('already guessed!')
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess in answer: 
+             for i in range(len(answer)):
+                  if guess == answer[i]:
+                       hint[i] = guess
+                       
+        else: 
+            Wrong_guesses += 1
+            print(f"{guess} is not in the word.")
+
+        if '_' not in hint:
+            display_hint(hint)
+            print("You Won!")
+            is_running = False
+        
+        if Wrong_guesses == 6:
+            display_hangman(Wrong_guesses)
+            is_running = False 
+            print("You Lost!")
+
+                       
+        
+        
+if __name__ == "__main__":
+    main()
+
+
